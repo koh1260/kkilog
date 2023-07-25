cdabfe65b0b60215982aaf01e991050d1210d611
Haesung <koh1260@naver.com>
2023-07-26 05:02:27 +0900

    refactor: Home 컴포넌트 컨테이너 태그 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,18 @@
+import { styled } from "styled-components";
 import Main from "../components/Main";
 import NavBar from "../components/NavBar";
 
+const Container = styled.article`
+  height: 100vh;
+  flex: 1;
+`
+
 const Home = () => {
   return (
-    <>
+    <Container>
       <NavBar />
       <Main />
-    </>
+    </Container>
   );
 };
 
