ce6b8bed48f9f09a2b6f0b97ce822f970d05291d
Haesung <koh1260@naver.com>
2023-07-26 07:45:17 +0900

    chore: 더미 데이터

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -27,6 +27,62 @@ export const posts = [
     introduction: "어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구",
     createAt: new Date(),
   },
+  {
+    id: 1,
+    title: "Nest.js 어쩌구 저쩌구구구구",
+    thumbnail: 'https://velog.velcdn.com/images/a001206/post/4c94f9f8-40d7-4d12-b07e-0ca6053556a5/image.png',
+    introduction: "어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구",
+    createAt: new Date(),
+  },
+	{
+    id: 2,
+    title: "Nest.js 어쩌구 저쩌구구구구",
+    thumbnail: 'https://velog.velcdn.com/images/a001206/post/4c94f9f8-40d7-4d12-b07e-0ca6053556a5/image.png',
+    introduction: "어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구",
+    createAt: new Date(),
+  },
+	{
+    id: 3,
+    title: "Nest.js 어쩌구 저쩌구구구구",
+    thumbnail: 'https://velog.velcdn.com/images/a001206/post/4c94f9f8-40d7-4d12-b07e-0ca6053556a5/image.png',
+    introduction: "어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구",
+    createAt: new Date(),
+  },
+	{
+    id: 4,
+    title: "Nest.js 어쩌구 저쩌구구구구",
+    thumbnail: 'https://velog.velcdn.com/images/a001206/post/4c94f9f8-40d7-4d12-b07e-0ca6053556a5/image.png',
+    introduction: "어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구",
+    createAt: new Date(),
+  },
+  {
+    id: 1,
+    title: "Nest.js 어쩌구 저쩌구구구구",
+    thumbnail: 'https://velog.velcdn.com/images/a001206/post/4c94f9f8-40d7-4d12-b07e-0ca6053556a5/image.png',
+    introduction: "어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구",
+    createAt: new Date(),
+  },
+	{
+    id: 2,
+    title: "Nest.js 어쩌구 저쩌구구구구",
+    thumbnail: 'https://velog.velcdn.com/images/a001206/post/4c94f9f8-40d7-4d12-b07e-0ca6053556a5/image.png',
+    introduction: "어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구",
+    createAt: new Date(),
+  },
+	{
+    id: 3,
+    title: "Nest.js 어쩌구 저쩌구구구구",
+    thumbnail: 'https://velog.velcdn.com/images/a001206/post/4c94f9f8-40d7-4d12-b07e-0ca6053556a5/image.png',
+    introduction: "어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구",
+    createAt: new Date(),
+  },
+	{
+    id: 4,
+    title: "Nest.js 어쩌구 저쩌구구구구",
+    thumbnail: 'https://velog.velcdn.com/images/a001206/post/4c94f9f8-40d7-4d12-b07e-0ca6053556a5/image.png',
+    introduction: "어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구",
+    createAt: new Date(),
+  },
 ];
 
 export const categorys = [
