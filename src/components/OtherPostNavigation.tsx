889a8b4c48fade21d9af4890ea2cd1d254543b06
Haesung <koh1260@naver.com>
2023-07-27 05:59:25 +0900

    feat: 다른 게시글 이동 컴포넌트 초안

--- src/components/OtherPostNavigation.tsx
+++ src/components/OtherPostNavigation.tsx
@@ -1,10 +1,80 @@
-import React from 'react';
+import React from "react";
+import { styled } from "styled-components";
+import nextArrow from '../assets/img/next-arrow.png';
+import previousArrow from '../assets/img/previous-arrow.png';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const NavigationBlock = styled.div`
+  background-color: #F8F9FA;
+  border-radius: 12px;
+  display: flex;
+  padding: 12px 14px;
+`;
+
+const StyledButton = styled.button`
+  background-color: transparent;
+  border: none;
+`;
+
+const Icon = styled.img`
+  width: 52px;
+  height: 52px;
+
+  &.previous {
+    margin-right: 12px;
+  }
+  &.next {
+    margin-left: 12px;
+  }
+`
+
+const OtherPost = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const PreviousPostText = styled.div``;
+
+const NextPostText = styled.div`
+  display: flex;
+  justify-content: end;
+  width: 100%;
+`;
+
+const OtherPostTitle = styled.h3`
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
 
 const OtherPostNavigation = () => {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <NavigationBlock>
+        <StyledButton>
+          <Icon className="previous" src={previousArrow}/>
+        </StyledButton>
+        <OtherPost>
+          <PreviousPostText>이전 글</PreviousPostText>
+          <OtherPostTitle>React 너무 어렵군!!</OtherPostTitle>
+        </OtherPost>
+      </NavigationBlock>
+
+      <NavigationBlock>
+        <OtherPost>
+          <NextPostText>다음 글</NextPostText>
+          <OtherPostTitle>React 너무 어렵군!!</OtherPostTitle>
+        </OtherPost>
+        <StyledButton>
+          <Icon className="next" src={nextArrow}/>
+        </StyledButton>
+      </NavigationBlock>
+    </Container>
   );
 };
 
