1be75d59b6f220683727e5d312c9b868980918bd
Haesung <koh1260@naver.com>
2023-07-28 21:04:08 +0900

    refactor: Comment 컴포넌트 재사용 컴포넌트로 변경

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import { styled } from 'styled-components';
-import {AiOutlineHeart} from 'react-icons/ai';
+import React from "react";
+import { styled } from "styled-components";
+import { AiOutlineHeart } from "react-icons/ai";
 // import {AiFillHeart} from 'react-icons/ai';
+
+interface CommentProps {
+  id: number;
+  nickname: string;
+  content: string;
+  createAt: Date;
+}
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 24px;
+  padding: 18px;
   border-radius: 12px;
-  border: 2px solid #F8F9FA;
-`
+  border: 2px solid #f8f9fa;
+`;
 
 const WriterInfo = styled.div`
   display: flex;
@@ -22,7 +29,7 @@ const WriterInfo = styled.div`
   .create-at {
     color: gray;
   }
-`
+`;
 
 const Profileimage = styled.img`
   width: 48px;
@@ -30,30 +37,30 @@ const Profileimage = styled.img`
   border-radius: 50%;
   border: 0.5px solid gray;
   margin-right: 14px;
-`
+`;
 
 const Content = styled.div`
   padding: 24px 0;
-`
+`;
 
 const LikeBlock = styled.div`
   display: flex;
   align-items: center;
   gap: 14px;
-`
+`;
 
-const Comment = () => {
+const Comment = ({ id, nickname, content, createAt }: CommentProps) => {
   return (
     <Container>
       <WriterInfo>
-        <Profileimage src='https://velog.velcdn.com/images/a001206/post/4c94f9f8-40d7-4d12-b07e-0ca6053556a5/image.png'/>
-        <p className='nickname'>닉네임</p>
-        <p className='create-at'>2020.10.10</p>
+        <Profileimage src="https://velog.velcdn.com/images/a001206/post/4c94f9f8-40d7-4d12-b07e-0ca6053556a5/image.png" />
+        <p className="nickname">{nickname}</p>
+        <p className="create-at">{`${createAt.getFullYear()}.${createAt.getMonth()}.${createAt.getDate()}`}</p>
       </WriterInfo>
-      <Content>코끼리가 쩌는군요!</Content>
+      <Content>{content}</Content>
       <LikeBlock>
-        <p className='like-count'>좋아요 1개</p>
-        <AiOutlineHeart size={'21px'}/>
+        <p className="like-count">좋아요 1개</p>
+        <AiOutlineHeart size={"21px"} />
       </LikeBlock>
     </Container>
   );
