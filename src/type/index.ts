4596952ce33c42a38c2e869870e8974e10049103
Haesung <koh1260@naver.com>
2023-08-13 19:57:49 +0900

    feat: 게시글, 댓글 타입 추가

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,8 +1,32 @@
-export interface Post {
+export interface PostPreview {
   id: number;
   title: string;
   introduction: string;
   thumbnail: string;
   createAt: Date;
   commentCount: number;
+};
+
+export interface Post {
+  id: number;
+  title: string;
+  content: string;
+  createAt: Date;
+  writer: Writer;
+  thumbnail: string;
+  comments: Comment[]
+}
+
+interface Writer {
+  nickname: string;
+  profileImage: string;
+}
+
+export interface Comment {
+  id: number;
+  profileImage: string;
+  nickname: string;
+  content: string;
+  createAt: Date;
+  writer: Writer;
 }
