6e11893b80fec335a2565691f2e6e07076dc15fe
Haesung <koh1260@naver.com>
2023-08-13 19:59:50 +0900

    feat: 반응형 속성 추가

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,24 +1,31 @@
-import React from "react";
-import { styled } from "styled-components";
-import PostContent from "./PostContent";
-import OtherPostNavigation from "./OtherPostNavigation";
-import CommentBlock from "./CommentBlock";
+import React from 'react';
+import { styled } from 'styled-components';
+import PostContent from './PostContent';
+import OtherPostNavigation from './OtherPostNavigation';
+import CommentBlock from './CommentBlock';
+import { Post } from '../type';
+
+interface DetailProps {
+  post: Post
+}
 
 const Container = styled.article`
   display: flex;
   flex-direction: column;
   width: 100%;
   height: 100%;
+  padding-left: 20px;
+  @media screen and (max-width: 1024px) {
+    padding: 0;
+  }
 `;
 
-const Detail = () => {
-  return (
+const Detail = ({ post }: DetailProps) => (
     <Container>
-      <PostContent />
+      <PostContent post={post}/>
       <OtherPostNavigation />
-      <CommentBlock />
+      <CommentBlock comments={post.comments}/>
     </Container>
-  );
-};
+  )
 
 export default Detail;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,5 @@
-import { styled } from "styled-components";
+import { styled } from 'styled-components';
+
 interface MainProps {
   children: JSX.Element;
 }
@@ -8,22 +9,24 @@ const Container = styled.main`
   display: flex;
   justify-content: center;
   height: 100%;
+  width: 100%;
   flex: 1;
 `;
 
 const PositionCenterBox = styled.div`
   max-width: 980px;
+  @media screen and (max-width: 1024px) {
+    width: 100%;
+  }
   margin: 0 auto;
 `;
 
-const Main = ({children}: MainProps) => {
-  return (
+const Main = ({children}: MainProps) => (
     <Container>
       <PositionCenterBox>
         {children}
       </PositionCenterBox>
     </Container>
-  );
-};
+  )
 
 export default Main;
