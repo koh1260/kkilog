9c56737a03339cad1fc6631a57a319986f22938b
Haesung <koh1260@naver.com>
2023-08-28 21:26:43 +0900

    feat: 앱 마운트 시 로그인 검증

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,32 @@ import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import GlobalStyle from './styles/GlobalStyle';
-import storage from './lib/storage';
 import { useAppDispatch } from './redux/hook';
-import { UserState, setLogined, setUser } from './redux/slice/user-slice';
+import { UserState, setUser } from './redux/slice/user-slice';
 import api from './api/api';
 
 const App = () => {
   const dispatch = useAppDispatch();
-  useEffect(() => {
-    const loginInfo = storage.get<UserState>('user');
-    
-    if (!loginInfo) return;
-    dispatch(setUser(loginInfo));
 
+  useEffect(() => {
     (async () => {
-      if (!loginInfo.username) return;
-      const response = await api.loginValidate(loginInfo.username);
-      if(response.status === 200) dispatch(setLogined({logined: true}))
+      try {
+        const token = localStorage.getItem('access_token');
+        if (!token) throw new Error('토큰이 없습니다');
+        const response = await api.loginValidate();
+        if (!response.result) throw new Error('회원 정보가 없습니다.');
+        const {result} = response;
+        const userInfo: UserState = {
+          id: result.id,
+          username: result.email,
+          role: result.role,
+          logined: true,
+        };
+        dispatch(setUser(userInfo));
+      } catch(e) {
+        if (e instanceof Error) console.log(`Error Stack: ${e.stack}`);
+      }
     })();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
