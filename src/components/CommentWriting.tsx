58e293bd773826fad7f69a0f674d0b40298153a7
Haesung <koh1260@naver.com>
2023-09-02 21:41:02 +0900

    feat: 비회원 댓글 작성 레이아웃 추가

--- src/components/CommentWriting.tsx
+++ src/components/CommentWriting.tsx
@@ -8,6 +8,22 @@ const Container = styled.div`
   margin-bottom: 24px;
 `;
 
+const WriterInfoBlock = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 12px;
+  margin: 12px 0;
+`;
+
+const WriterInfoInput = styled.input`
+  width: 25%;
+  padding: 0.4rem 1rem;
+  font-size: 1rem;
+  outline: none;
+  border: 2px solid #F8F9FA;
+  border-radius: 7px;
+`;
+
 const CommentInput = styled.textarea`
   width: 100%;
   min-height: 6rem;
@@ -34,6 +50,10 @@ const CommentWriting = () => {
   const [value, setValue] = useState('');
   return (
     <Container>
+      <WriterInfoBlock>
+        <WriterInfoInput placeholder='닉네임' />
+        <WriterInfoInput type='password' placeholder='비밀번호' />
+      </WriterInfoBlock>
       <CommentInput value={value} placeholder='댓글을 입력해주세요' onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setValue(e.target.value);
       }}/>
