80ac58a705ee3e53395241ca9ae89e1f14e3f506
Haesung <koh1260@naver.com>
2023-09-23 20:58:17 +0900

    fix: date 타입 포맷 변경 시 month가 1적게 출력되는 버그 수정

--- src/lib/format-date.ts
+++ src/lib/format-date.ts
@@ -1,3 +1,3 @@
-const formatDate = (date: Date) => `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDate()}일`;
+const formatDate = (date: Date) => `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`;
 
 export default formatDate;
