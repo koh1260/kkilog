32c1b66d61fc48a90abe48d2a9c513bb31c713ea
Haesung <koh1260@naver.com>
2023-11-26 20:44:37 +0900

    feat: 댓글 작성 시 최하단으로 스크롤

--- src/components/CommentList.tsx
+++ src/components/CommentList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { styled } from 'styled-components';
 import Comment from './Comment';
 import { Comment as IComment } from '../type';
@@ -6,6 +6,7 @@ import stringToDate from '../lib/transform-date';
 
 interface CommentListProps {
   comments: IComment[];
+  scrollBottom(): void;
 }
 
 const Container = styled.div`
@@ -14,7 +15,12 @@ const Container = styled.div`
   gap: 21px 0;
 `;
 
-const CommentList = ({comments}: CommentListProps) => (
+const CommentList = ({ comments, scrollBottom }: CommentListProps) => {
+  useEffect(() => {
+    scrollBottom();
+  }, [comments])
+
+  return (
     <Container>
       {comments.map((comment) => (
         <Comment
@@ -22,11 +28,16 @@ const CommentList = ({comments}: CommentListProps) => (
           id={comment.id}
           nickname={comment.writer ? comment.writer.nickname : comment.nickname}
           content={comment.content}
-          profileImage={comment.writer ? comment.writer.profileImage : 'https://cdn.dribbble.com/users/690037/screenshots/16596083/elephant_logo-01_4x.jpg'}
+          profileImage={
+            comment.writer
+              ? comment.writer.profileImage
+              : 'https://cdn.dribbble.com/users/690037/screenshots/16596083/elephant_logo-01_4x.jpg'
+          }
           createAt={stringToDate(comment.createAt)}
         />
       ))}
     </Container>
-  )
+  );
+};
 
 export default CommentList;
