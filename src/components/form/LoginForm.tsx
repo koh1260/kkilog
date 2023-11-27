30f27b1866791cf6597ad6ad40dc5a3fae4d7a88
Haesung <koh1260@naver.com>
2023-11-27 20:21:02 +0900

    chore: 주석 제거

--- src/components/form/LoginForm.tsx
+++ src/components/form/LoginForm.tsx
@@ -22,17 +22,6 @@ const Container = styled.form`
   }
 `;
 
-// const LoginButton = styled.button`
-//   height: 2.5rem;
-//   padding: 0.4rem 1rem;
-//   color: white;
-//   background-color: #529fdc;
-//   border: none;
-//   font-size: 1.2rem;
-//   font-weight: 500;
-//   width: fit-content;
-// `;
-
 const LoginButtonBlock = styled.div`
   margin-top: 1.2rem;
   display: flex;
@@ -85,7 +74,7 @@ const LoginForm = () => {
         storage.set('user', loginedUser);
         dispatch(setIsVisibleLoginModal({isVisibleLoginModal: false}))
         document.body.classList.remove('open-modal');
-        navigate('/');  
+        navigate('/');
       }
     } catch (e: any) {
       if (e instanceof ClientExcepction) console.error(e.stack);
