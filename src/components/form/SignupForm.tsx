d45dd6ab6b362ed9ec5b9bc9a3eb54378ff7afeb
Haesung <koh1260@naver.com>
2023-11-27 20:51:27 +0900

    feat: 회원가입 데이터 유효성 검사

--- src/components/form/SignupForm.tsx
+++ src/components/form/SignupForm.tsx
@@ -16,6 +16,13 @@ const Container = styled.form`
   }
 `;
 
+const ErrorMessage = styled.div`
+  min-width: 18rem;
+  color: red;
+  font-size: 0.8rem;
+  font-weight: 600;
+`;
+
 interface SignupInfo {
   email: string;
   nickname: string;
@@ -23,11 +30,27 @@ interface SignupInfo {
 }
 
 const SignupForm = () => {
+  const [errorMessage, setErrorMessage] = useState('');
   const [payload, setPayload] = useState<SignupInfo>({
     email: '',
     nickname: '',
     password: ''
   });
+
+  const checkBlank = () =>
+    payload.email && payload.nickname && payload.password;
+
+  const checkEmail = () => {
+    const reg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return reg.test(payload.email);
+  };
+
+  const checkNickname = () => payload.nickname.length < 16;
+
+  const checkPassword = () => {
+    const reg = /^[A-Za-z\\d!@#$%^&*()]{8,30}$/;
+    return reg.test(payload.password);
+  };
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -39,6 +62,22 @@ const SignupForm = () => {
 
   const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!checkBlank()) {
+      setErrorMessage('입력란을 모두 입력해주세요.');
+      return;
+    }
+    if (!checkEmail()) {
+      setErrorMessage('이메일 형식이 일치하지 않습니다.');
+      return;
+    }
+    if (!checkNickname()) {
+      setErrorMessage('닉네임은 최대 15자입니다.');
+      return;
+    }
+    if (!checkPassword()) {
+      setErrorMessage('비밀번호는 8~30자이며 특수문자를 포함할 수 있습니다.');
+      return;
+    }
 
     try {
       const response = await api.signup(payload);
@@ -47,8 +86,8 @@ const SignupForm = () => {
         return;
       }
 
-      const er: ErrorResponse = await response.json();
-      alert(er.message);
+      const errorObj: ErrorResponse = await response.json();
+      setErrorMessage(errorObj.message);
     } catch (error) {
       if (error instanceof Error) console.error(error.stack);
     }
@@ -75,6 +114,7 @@ const SignupForm = () => {
         value={payload.password}
         onChange={handleOnChange}
       />
+      <ErrorMessage>{errorMessage}</ErrorMessage>
       <StyledButton>회원가입</StyledButton>
     </Container>
   );
