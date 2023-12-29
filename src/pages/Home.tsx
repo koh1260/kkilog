75bf56061739fbba515b9c7385f96968c9f3a4d2
Haesung <koh1260@naver.com>
2023-12-29 15:13:22 +0900

    refactor: 불필요하게 분리한 컴포넌트 병합

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import { styled } from 'styled-components';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import Main from '../components/Main';
 import NavBar from '../components/NavBar';
 import CategoryBar from '../components/CategoryBar';
 import PostList from '../components/PostList';
 import { PostPreview } from '../type';
-import BodyContainer from '../components/BodyContainer';
 import api from '../api/api';
 import Loading from '../components/Loading';
 import ClientExcepction from '../common/exceptions/client-exception';
@@ -20,23 +18,34 @@ const Container = styled.article`
   flex: 1;
 `;
 
-const BodyBlock = styled.div`
-  display: flex;
+const Body = styled.div`
+  width: 100%;
   height: 100%;
-  
+`;
+
+const ContentBlock = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+
   @media screen and (max-width: 1285px) {
     flex-direction: column;
   }
 `;
 
+const PostListBlock = styled.div`
+  flex: 1;
+  padding: 4rem 2rem;
+`;
+
 const Home = () => {
-  const {categoryName} = useParams<'categoryName'>();
+  const { categoryName } = useParams<'categoryName'>();
   const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState<PostPreview[]>([]);
 
   useEffect(() => {
-    window.scrollTo(0,0);
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
 
   useEffect(() => {
     const getPosts = async (name: string | undefined) => {
@@ -51,30 +60,29 @@ const Home = () => {
       } catch (e) {
         if (e instanceof ClientExcepction) {
           console.error(e.stack);
-        }
-        else if (e instanceof Error) {
+        } else if (e instanceof Error) {
           console.error(e.stack);
         }
       }
     };
     getPosts(categoryName);
     setLoading(false);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [categoryName]);
 
-  if (loading) return (<Loading />)
+  if (loading) return <Loading />;
 
   return (
     <Container>
       <NavBar />
-      <BodyContainer>
-        <BodyBlock>
+      <Body>
+        <ContentBlock>
           <CategoryBar />
-          <Main>
+          <PostListBlock>
             <PostList categoryName='Total' posts={posts} />
-          </Main>
-        </BodyBlock>
-      </BodyContainer>
+          </PostListBlock>
+        </ContentBlock>
+      </Body>
       <Footer />
     </Container>
   );
