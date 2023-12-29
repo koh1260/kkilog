5cb4cd9e880845bde9294c27f048e57a8d344134
Haesung <koh1260@naver.com>
2023-12-29 21:12:10 +0900

    chore: 실수로 추가한 코드 삭제

--- src/components/CommentWriting.tsx
+++ src/components/CommentWriting.tsx
@@ -5,7 +5,7 @@ import api from '../api/api';
 import { useAppDispatch, useAppSelector } from '../redux/hook';
 import ClientExcepction from '../common/exceptions/client-exception';
 import { Comment } from '../type';
-import { openLoginModal, setIsVisibleLoginModal } from '../redux/slice/modal-slice';
+import { openLoginModal } from '../redux/slice/modal-slice';
 
 interface CommentWritingProps {
   setCommentList: React.Dispatch<React.SetStateAction<Comment[]>>;
@@ -61,7 +61,6 @@ const CommentWriting = ({
       setCommentList(response.result);
       scrollBottom();
     }
-    dispatch(setIsVisibleLoginModal({isVisibleLoginModal: true}));
   };
 
   const handleOnClickPost = async () => {
