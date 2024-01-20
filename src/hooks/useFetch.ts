5a52c393c13d312f60b72e8518b945390d7dc3d7
Haesung <koh1260@naver.com>
2024-01-21 05:25:12 +0900

    feat: useFetch 훅 구현

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+function useFetch<TData>(
+  callback: () => Promise<TData>,
+) {
+  const [data, setData] = useState<TData>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    callback()
+      .then((result) => setData(result))
+      .catch((e) => {
+        setIsError(true);
+        setError(e);
+      })
+      .finally(() => setIsLoading(false));
+  }, [callback]);
+
+  if (!data) return { isLoading, isError, error };
+  return { data, isLoading, isError, error };
+}
+
+export default useFetch;
