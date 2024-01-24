e169cfbd68ae2ba7357405408854dec9b81d6fb7
Haesung <koh1260@naver.com>
2024-01-25 07:57:44 +0900

    feat: react-query 도입

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import router from './router';
 import GlobalStyle from './styles/GlobalStyle';
 import { useAppDispatch } from './redux/hook';
@@ -7,6 +8,17 @@ import { UserState, setUser } from './redux/slice/user-slice';
 import api from './api/api';
 
 const App = () => {
+  const queryClient = new QueryClient(
+    {
+    defaultOptions: {
+      queries: {
+        throwOnError: true,
+        retry: 0
+      }
+    }
+  }
+  );
+
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -14,25 +26,25 @@ const App = () => {
       try {
         const response = await api.loginValidate();
         if (!response.result) throw new Error('회원 정보가 없습니다.');
-        const {result} = response;
+        const { result } = response;
         const userInfo: UserState = {
           id: result.id,
           username: result.email,
           role: result.role,
-          logined: true,
+          logined: true
         };
         dispatch(setUser(userInfo));
-      } catch(e) {
+      } catch (e) {
         if (e instanceof Error) console.log(`${e.stack}`);
       }
     })();
   }, []);
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   );
 };
 
