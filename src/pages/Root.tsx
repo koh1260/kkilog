2f9e625baad2126e1528b6a243f1b9d263eb3e98
Haesung <koh1260@naver.com>
2024-01-28 07:09:50 +0900

    chore: 주석 코드 제거

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,41 +1,15 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Loading from '../components/Loading';
-import QueryErrorBoundary from '../components/error/QueryErrorBoundary';
 import useAuthValidation from '../hooks/useAuthValidation';
 
 const Root = () => {
   useAuthValidation();
 
-  // useAuthValidation();
-
-  // const dispatch = useAppDispatch();
-
-  // useEffect(() => {
-  //   (async () => {
-  //     try {
-  //       const response = await api.loginValidate();
-  //       if (!response.result) throw new Error('회원 정보가 없습니다.');
-  //       const { result } = response;
-  //       const userInfo: UserState = {
-  //         id: result.id,
-  //         username: result.email,
-  //         role: result.role,
-  //         logined: true
-  //       };
-  //       dispatch(setUser(userInfo));
-  //     } catch (e) {
-  //       if (e instanceof Error) console.log(`${e.stack}`);
-  //     }
-  //   })();
-  // }, []);
-
   return (
-    <QueryErrorBoundary>
-      <Suspense fallback={<Loading />}>
-        <Outlet />
-      </Suspense>
-    </QueryErrorBoundary>
+    <Suspense fallback={<Loading />}>
+      <Outlet />
+    </Suspense>
   );
 };
 
