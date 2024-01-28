1a23311e59f823d7edddb22b6b21d8066b3106ae
Haesung <koh1260@naver.com>
2024-01-28 10:38:56 +0900

    style: 테마 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'styled-components';
 import router from './router';
 import GlobalStyle from './styles/GlobalStyle';
+import theme from './styles/theme';
 
 const App = () => {
-  const queryClient = new QueryClient(
-    {
+  const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
         throwOnError: true,
         retry: 0
       }
     }
-  }
-  );
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
-      <GlobalStyle />
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </QueryClientProvider>
   );
 };
