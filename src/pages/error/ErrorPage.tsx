aa14afae18c65549cadd05e343015c3f4d2f2fed
Haesung <koh1260@naver.com>
2024-01-28 10:38:21 +0900

    feat: 홈으로 이동 버튼 추가

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useRouteError } from 'react-router-dom';
+import { useRouteError, Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -18,6 +17,17 @@ const Icon = styled.img`
   width: 20rem;
 `;
 
+const HomeButton = styled(Link)`
+  margin: 2rem 0;
+  font-size: 1.2rem;
+  font-weight: 600;
+  border: none;
+  border-radius: 7px;
+  padding: 0.4rem 0.6rem;
+  color: white;
+  background-color: ${({ theme }) => theme.color.primary};
+`;
+
 const ErrorPage = () => {
   const error = useRouteError();
   const message = error instanceof Error ? error.message : '알 수 없는 에러입니다.';
@@ -25,6 +35,7 @@ const ErrorPage = () => {
   return (
     <Container>
       <ErrorMessage>{message}</ErrorMessage>
+      <HomeButton to='/'>집으로!</HomeButton>
       <Icon src='https://haesungsbucket.s3.ap-northeast-2.amazonaws.com/kkilog/error-elephant.png' />
     </Container>
   );
--- src/pages/error/NotFoundPage.tsx
+++ src/pages/error/NotFoundPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -11,16 +12,27 @@ const Container = styled.div`
 
 const ErrorMessage = styled.h1`
   font-size: 4.6rem;
-  margin-bottom: 5rem;
 `;
 
 const Icon = styled.img`
   width: 20rem;
 `;
 
+const HomeButton = styled(Link)`
+  margin: 2rem 0;
+  font-size: 1.2rem;
+  font-weight: 600;
+  border: none;
+  border-radius: 7px;
+  padding: 0.4rem 0.6rem;
+  color: white;
+  background-color: ${({ theme }) => theme.color.primary};
+`;
+
 const NotFoundPage = () => (
   <Container>
     <ErrorMessage>404</ErrorMessage>
+    <HomeButton to='/'>집으로!</HomeButton>
     <Icon src='https://haesungsbucket.s3.ap-northeast-2.amazonaws.com/kkilog/error-elephant.png' />
   </Container>
 );
